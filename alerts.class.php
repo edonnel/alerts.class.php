cb1b4460e08994937853db47b7b2de28dfab84f6
Evan Donnel <35748349+edonnel@users.noreply.github.com>
2021-08-29 22:05:21 -0400

    Update alerts.class.php

--- alerts.class.php
+++ alerts.class.php
@@ -1,66 +1,66 @@
 <?
 	class alerts {
 
-			public static function push($text, $title = '', $type = 'alert', $redirect = false, $critical = false) {
-				if (isset($_SESSION['ed_alerts']) && !is_array($_SESSION['ed_alerts']) || !isset($_SESSION['ed_alerts']))
-					$_SESSION['ed_alerts'] = array();
+		public static function push($text, $title = '', $type = 'alert', $redirect = false, $critical = false) {
+			if (isset($_SESSION['ed_alerts']) && !is_array($_SESSION['ed_alerts']) || !isset($_SESSION['ed_alerts']))
+				$_SESSION['ed_alerts'] = array();
 
-				array_push($_SESSION['ed_alerts'], array(
-					'text'      => $text,
-					'title'     => $title,
-					'type'      => $type,
-					'critical'  => $critical,
-				));
+			array_push($_SESSION['ed_alerts'], array(
+				'text'      => $text,
+				'title'     => $title,
+				'type'      => $type,
+				'critical'  => $critical,
+			));
 
-				if ($redirect) {
-					if (!headers_sent())
-						header('Location: '.$redirect);
-					else
-						echo '<script type="text/javascript">window.location="'.$redirect.'"</script>';
+			if ($redirect) {
+				if (!headers_sent())
+					header('Location: '.$redirect);
+				else
+					echo '<script type="text/javascript">window.location="'.$redirect.'"</script>';
 
-					die();
-				}
+				die();
 			}
+		}
 
-			public static function get($only_first = false) {
-				$output = '';
+		public static function get($only_first = false) {
+			$output = '';
 
-				if (isset($_SESSION['ed_alerts']) && is_array($_SESSION['ed_alerts']) && count($_SESSION['ed_alerts']) > 0) {
-					$output .= '<div class="ed-alerts">';
+			if (isset($_SESSION['ed_alerts']) && is_array($_SESSION['ed_alerts']) && count($_SESSION['ed_alerts']) > 0) {
+				$output .= '<div class="ed-alerts">';
 
-					foreach ($_SESSION['ed_alerts'] as $alert) {
-						$output .= '<div class="ed-alert '.$alert['type'].'">';
-						$output .= '<div class="fa fa-fw fa-close close" style="float:right; line-height:inherit; cursor:pointer;" onclick="RemoveEDAlert(this);"></div>';
+				foreach ($_SESSION['ed_alerts'] as $alert) {
+					$output .= '<div class="ed-alert '.$alert['type'].'">';
+					$output .= '<div class="fa fa-fw fa-close close" style="float:right; line-height:inherit; cursor:pointer;" onclick="RemoveEDAlert(this);"></div>';
 
-						if ($alert['title'])
-							$output .= '<div class="ed-alert-title">'.$alert['title'].'</div>';
+					if ($alert['title'])
+						$output .= '<div class="ed-alert-title">'.$alert['title'].'</div>';
 
-						$output .= '<div class="ed-alert-text">'.$alert['text'].'</div>';
-						$output .= '</div>';
-
-						if ($only_first)
-							break;
-					}
-
+					$output .= '<div class="ed-alert-text">'.$alert['text'].'</div>';
 					$output .= '</div>';
 
-					$output .= '<script>function RemoveEDAlert(that){$(that).parent().slideUp();}</script>';
-
-					unset($_SESSION['ed_alerts']);
+					if ($only_first)
+						break;
 				}
 
-				return $output;
+				$output .= '</div>';
+
+				$output .= '<script>function RemoveEDAlert(that){$(that).parent().slideUp();}</script>';
+
+				unset($_SESSION['ed_alerts']);
 			}
 
-			public static function are_critical() {
-				if (isset($_SESSION['ed_alerts']) && $_SESSION['ed_alerts']) {
-					foreach ($_SESSION['ed_alerts'] as $alert) {
-						if (isset($alert['critical']) && $alert['critical'])
-							return true;
-					}
-				}
-
-				return false;
-			}
-
+			return $output;
 		}
+
+		public static function are_critical() {
+			if (isset($_SESSION['ed_alerts']) && $_SESSION['ed_alerts']) {
+				foreach ($_SESSION['ed_alerts'] as $alert) {
+					if (isset($alert['critical']) && $alert['critical'])
+						return true;
+				}
+			}
+
+			return false;
+		}
+
+	}
